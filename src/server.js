5c1898b6347757ec501149d2272fe3b42e5e9c32
RokasV <vezikauskas.r@gmail.com>
2023-12-19 09:39:59 +0200

    env ignore commit

--- src/server.js
+++ src/server.js
@@ -1,20 +1,50 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
+const { port } = require('./config');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
+const pass = process.env.PASS;
+const users = [
+  {
+    id: 1,
+    name: 'Serbentautas',
+    town: 'Vilnius',
+    isDeleted: false,
+  },
+  {
+    id: 2,
+    name: 'Lenteja',
+    town: 'Kaunas',
+    isDeleted: false,
+  },
+  {
+    id: 3,
+    name: 'James',
+    town: 'London',
+    isDeleted: false,
+  },
+];
 
 // Middleware
-
 app.use(cors());
 app.use(morgan('dev'));
 
+// ROUTES
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+// GET /api/users - grazina visus vartotojus
+app.get('/api/users', (req, res) => {
+  res.json(users);
 });
+
+app.listen(PORT, () => {
+  console.log(`Server runing on http://localhost:${PORT}`);
+});
+// app.listen(PORT);
