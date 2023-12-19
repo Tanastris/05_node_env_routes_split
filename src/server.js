73216c5d436282bc69f7a41a716d87c07d44eccd
RokasV <vezikauskas.r@gmail.com>
2023-12-19 09:11:34 +0200

    lint

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// Middleware
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
 app.listen(PORT, () => {
-  `Server running on http://localhost:${PORT}`;
+  console.log(`Server running on http://localhost:${PORT}`);
 });
